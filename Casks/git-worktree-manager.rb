93190648faa38e61323c23266a08605211c12a87
zixiaoyou <zixiaoyou@MacBook-Pro.local>
2026-04-10 13:35:55 +0800

    fix: 修复 Cask 文件中的字符串插值语法

--- Casks/git-worktree-manager.rb
+++ Casks/git-worktree-manager.rb
@@ -3,11 +3,11 @@ cask "git-worktree-manager" do
 
   on_macos do
     on_intel do
-      url "https://github.com/zhuSilence/git-worktree-manager/releases/download/v\#{version}/Git.Worktree.Manager_\#{version}_x64.dmg"
+      url "https://github.com/zhuSilence/git-worktree-manager/releases/download/v#{version}/Git.Worktree.Manager_#{version}_x64.dmg"
       sha256 "27b4574c89fc362e74b179111d33fc88042ff1761507962ce92b16847e0f1194"
     end
     on_arm do
-      url "https://github.com/zhuSilence/git-worktree-manager/releases/download/v\#{version}/Git.Worktree.Manager_\#{version}_aarch64.dmg"
+      url "https://github.com/zhuSilence/git-worktree-manager/releases/download/v#{version}/Git.Worktree.Manager_#{version}_aarch64.dmg"
       sha256 "98fe5ca13fb3b699b05c89da4fd991825cfa35be5c65f279115b56b8380e3e6c"
     end
   end
@@ -25,7 +25,7 @@ cask "git-worktree-manager" do
 
   app "Git Worktree Manager.app"
 
-  binary "\#{appdir}/Git Worktree Manager.app/Contents/MacOS/git-worktree-manager"
+  binary "#{appdir}/Git Worktree Manager.app/Contents/MacOS/git-worktree-manager"
 
   zap trash: [
     "~/Library/Application Support/git-worktree-manager",
